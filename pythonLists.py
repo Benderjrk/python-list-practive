1beb23882f00f596cd36cf2dbf80e6b6b88352dc
Justin Bender <jrkbender@gmail.com>
2020-04-26 14:15:24 -0400

    code academy project code

--- pythonLists.py
+++ pythonLists.py
@@ -48,3 +48,25 @@ inventory_2_6 = inventory[2:6]
 first_3 = inventory[:3]
 twin_beds = inventory.count('twin bed')
 inventory.sort()
+
+toppings = ['pepperoni', 'pineapple', 'cheese', 'sausage', 'olives', 'anchovies', 'mushrooms']
+
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_pizzas = len(toppings)
+print("We sell " +str(num_pizzas)+ " different kinds of pizza!")
+
+pizzas = list(zip(prices, toppings))
+print(pizzas)
+pizzas.sort()
+print(pizzas)
+cheapest_pizza = pizzas[0]
+print(cheapest_pizza)
+priciest_pizza = pizzas[-1]
+print(priciest_pizza)
+
+three_cheapest = pizzas[:3]
+print(three_cheapest)
+
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
